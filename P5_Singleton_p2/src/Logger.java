13010fb331d9653d9574e9ca67c2390bc7fe0f96
Arazm1 <araz.muhamm3d@gmail.com>
2026-02-10 18:21:08 +0200

    Practices related to BufferedWriter. Understand the process.

--- P5_Singleton_p2/src/Logger.java
+++ P5_Singleton_p2/src/Logger.java
@@ -6,46 +6,66 @@ import java.io.IOException;
 
 public class Logger {
     private static Logger instance;
+    private BufferedWriter bw;
     private String filename;
 
-    private BufferedWriter bw;
 
-
-    private Logger(String filename){
-        this.filename = filename;
+    private Logger(){
+        setFilename("P5_Singleton_p2/textfiles/filename.txt");
 
     }
 
     public static synchronized Logger getInstance(){
         if (instance == null) {
-            instance = new Logger("P5_Singleton_p2/src/filename.txt");
+            instance = new Logger();
         }
         return instance;
     }
 
 
     public synchronized void setFilename(String newFilename){
-        this.filename = newFilename;
-    }
-
-    
-
-
-    
-
-    public void write(String message){
         try{
-            bw = new BufferedWriter(new FileWriter(filename, true));
-            bw.write(message);
-            bw.newLine();
-            bw.close();
-            System.out.println("Successfull!");
-            
-
-
+            close();
+            this.filename = newFilename;
+            bw = new BufferedWriter(new FileWriter("P5_Singleton_p2/textfiles/"+ filename, true));
         }
         catch(IOException e){
             System.out.println("Error: " + e);
+        }
+        catch(Error e){
+            e.printStackTrace();
+        }        
+    }
+
+    public void write(String message){
+        try{
+            if(bw != null){
+                bw.write(message);
+                bw.newLine();
+                bw.flush();
+                System.out.println("Successfull!");
+            }   
+        }
+        catch(IOException e){
+            System.out.println("Error: " + e);
+        }
+        catch(Error e){
+            System.out.println("Error " + e);
+        }
+    }
+
+    public synchronized void close(){
+        try{
+            if(bw != null){
+                bw.close();
+                bw = null;
+            }
+        }
+        catch(IOException e){
+            System.out.println("Error closing file: " + e);
+        }
+        catch(Error e){
+            e.printStackTrace();
         }
 
     }
--- P5_Singleton_p2/src/Main.java
+++ P5_Singleton_p2/src/Main.java
@@ -3,6 +3,7 @@ package P5_Singleton_p2.src;
 public class Main {
     public static void main(String[] args) {
         Logger logger = Logger.getInstance();
+        logger.setFilename("File1.txt");
         logger.write("Hi. First Line");
         logger.write("Bye. Second Line");
         
